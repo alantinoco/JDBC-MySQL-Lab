e0fa42ea2803c2ed6c89204bc9abce239e4d6d5f
Alan <alantinoco@gmail.com>
2022-11-14 17:30:42 -0300

    connection ok

--- JDBCLab/src/main/java/org/example/Main.java
+++ JDBCLab/src/main/java/org/example/Main.java
@@ -1,7 +1,32 @@
 package org.example;
 
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException classError){
+            System.out.println("Error :" + classError);
+        }
+
+        try{
+            Connection connection = DriverManager.getConnection("jdbc:mysql://172.17.0.2:3306/TheDoctorDB", "root", "password");
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM actors;");
+            while(resultSet.next()){
+                System.out.println(resultSet.getString("firstName")+ " " + resultSet.getString("lastName"));
+            }
+            connection.close();
+        }catch(SQLException sqlError){
+            System.out.println("Error :" + sqlError);
+        }
     }
+
 }
